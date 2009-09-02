712c154b2f15dcf2def4d1e4985c4a1ead1e2afe
kaarenilsen <kaare.nilsen@gmail.com>
2009-09-02 22:40:00 +0200

    o Removed tags attribute on @Environment, was kind a not that clever in the first plase. BUT this is a change of the public API, and will potentially break older apps o added test for autwiring in ConstrettoSpringJunit4Runner

---
+++ constretto-test/src/test/java/org/constretto/test/helper/Green.java
@@ -0,0 +1,7 @@
+package org.constretto.test.helper;
+
+/**
+ * @author <a href="mailto:kaare.nilsen@arktekk.no">Kaare Nilsen</a>
+ */
+public class Green {
+}
---
+++ constretto-test/src/test/java/org/constretto/test/helper/Red.java
@@ -0,0 +1,15 @@
+package org.constretto.test.helper;
+
+import org.springframework.stereotype.Component;
+import org.constretto.spring.annotation.Environment;
+
+/**
+ * @author <a href="mailto:kaare.nilsen@arktekk.no">Kaare Nilsen</a>
+ */
+@Component
+@Environment("springjunit")
+public class Red implements Color{
+    public String name() {
+        return "green";
+    }
+}
