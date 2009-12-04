fcd10652f9288dded14200540ece117d1f989b8f
kaarenilsen <kaare.nilsen@gmail.com>
2009-12-04 11:36:35 +0100

    o forgot a class in last commit set

--- constretto-api/src/main/java/org/constretto/Property.java
+++ constretto-api/src/main/java/org/constretto/Property.java
@@ -4,4 +4,47 @@ package org.constretto;
  * @author <a href="mailto:kaare.nilsen@arktekk.no">Kaare Nilsen</a>
  */
 public class Property {
+    private final String key;
+    private final String value;
+
+    public Property(String key, String value) {
+        this.value = value;
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Property)) return false;
+
+        Property property = (Property) o;
+
+        if (!key.equals(property.key)) return false;
+        if (!value.equals(property.value)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Property{" +
+                "key='" + key + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
 }
