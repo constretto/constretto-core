882d0521eff36bd811206362186a27f5ec8fb0f5
kaarenilsen <kaare.nilsen@gmail.com>
2010-06-02 22:32:29 +0200

    o added support for getting from system env as well as system prop to make it consistent with how it is done other places in Constretto

--- constretto-core/src/main/java/org/constretto/internal/store/EncryptedPropertiesStore.java
+++ constretto-core/src/main/java/org/constretto/internal/store/EncryptedPropertiesStore.java
@@ -1,5 +1,6 @@
 package org.constretto.internal.store;
 
+import org.jasypt.encryption.pbe.PBEStringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.properties.EncryptableProperties;
 
@@ -8,26 +9,34 @@ import java.util.Properties;
 /**
  * Has support for jasypt encrypted properties.
  * See  <a href="http://www.jasypt.org/cli.html">Jasypt CLI</a> and <a href="http://www.jasypt.org/encrypting-configuration.html">Jasypt Configuration Files</a>
+ *
  * @author Ole-Martin Mørk (olemartin@openadex.com)
  */
 public class EncryptedPropertiesStore extends PropertiesStore {
-    private StandardPBEStringEncryptor encryptor;
+    private final PBEStringEncryptor encryptor;
 
     /**
      * Creates a new instance
+     *
      * @param passwordProperty the name of the system property to read from
      */
     public EncryptedPropertiesStore(String passwordProperty) {
         encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword(getPassword(passwordProperty));
+        encryptor.setPassword(getFromSystemPropertyOrSystemEnv(passwordProperty));
     }
 
-    private String getPassword(String property) {
-        return System.getProperty(property);
+
+    private String getFromSystemPropertyOrSystemEnv(String key) {
+        String password = System.getProperty(key);
+        if (password == null) {
+            password = System.getenv(key);
+        }
+        return password;
     }
 
     /**
      * Uses jasypt to parse the properties
+     *
      * @param props the properties currently read
      * @return an instance of {@link org.jasypt.properties.EncryptableProperties}
      */
