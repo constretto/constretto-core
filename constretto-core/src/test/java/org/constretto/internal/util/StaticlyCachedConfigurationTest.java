5be1717775e5c21d47f7170757ab367e50386a32
kaarenilsen <kaare.nilsen@gmail.com>
2011-02-26 17:00:16 +0100

    o added a staticly cached configuration factory. for Finntech

---
+++ constretto-core/src/test/java/org/constretto/internal/util/StaticlyCachedConfigurationTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.constretto.internal.util;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.constretto.util.StaticlyCachedConfiguration.cacheHits;
+import static org.constretto.util.StaticlyCachedConfiguration.config;
+
+/**
+ * @author <a href="mailto:kaare.nilsen@gmail.com">Kaare Nilsen</a>
+ */
+public class StaticlyCachedConfigurationTest {
+
+    @Test
+    public void cachedSingleIniFile() {
+        String value = config("classpath:cache1.ini").evaluateToString("key1");
+        assertEquals("value1", value);
+        assertEquals(0, cacheHits());
+        value = config("classpath:cache1.ini").evaluateToString("key1");
+        assertEquals("value1", value);
+        assertEquals(1, cacheHits());
+    }
+
+    @Test
+    public void cachedMultipleIniFile() {
+        String value = config("classpath:cache1.ini", "classpath:cache2.ini").evaluateToString("key1");
+        assertEquals("value1", value);
+        String value2 = config("classpath:cache1.ini", "classpath:cache2.ini").evaluateToString("key2");
+        assertEquals("value2", value2);
+    }
+
+    @Test
+    public void cachedMultipleFilesWithDifferentType() {
+        String value = config("classpath:cache1.ini", "classpath:cache2.ini", "classpath:cache3.properties").evaluateToString("key1");
+        assertEquals("value1", value);
+        String value2 = config("classpath:cache1.ini", "classpath:cache2.ini", "classpath:cache3.properties").evaluateToString("key2");
+        assertEquals("value2", value2);
+        String value3 = config("classpath:cache1.ini", "classpath:cache2.ini", "classpath:cache3.properties").evaluateToString("key3");
+        assertEquals("value3", value3);
+    }
+
+    @Test
+    public void cachedSinglePropertyFile() {
+        String value = config("classpath:cache3.properties").evaluateToString("key3");
+        assertEquals("value3", value);
+    }
+}
