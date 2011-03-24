2dec2d3cf1fd58d6967a9d7f991ca0b56d721306
kaarenilsen <kaare.nilsen@gmail.com>
2011-03-24 14:17:07 +0100

    o applied convert enum patch

--- constretto-core/src/main/java/org/constretto/internal/converter/ValueConverterRegistry.java
+++ constretto-core/src/main/java/org/constretto/internal/converter/ValueConverterRegistry.java
@@ -15,6 +15,7 @@
  */
 package org.constretto.internal.converter;
 
+import org.constretto.exception.ConstrettoConversionException;
 import org.constretto.exception.ConstrettoException;
 import org.springframework.core.io.Resource;
 
@@ -65,10 +66,21 @@ public class ValueConverterRegistry {
     public static <T> T convert(Class<T> clazz, String value) throws ConstrettoException {
 
         if (!converters.containsKey(clazz)) {
-            throw new ConstrettoException("No converter found for class: " + clazz.getName());
+            if (!Enum.class.isAssignableFrom(clazz)) {
+                throw new ConstrettoException("No converter found for class: " + clazz.getName());
+            }
+
+            return (T) convertEnum((Class) clazz, value);
         }
         ValueConverter<?> converter = converters.get(clazz);
         return (T) converter.fromString(value);
     }
 
+    private static <T extends Enum<T>> T convertEnum(Class<T> clazz, String value) {
+        try {
+            return Enum.valueOf(clazz, value);
+        } catch (IllegalArgumentException e) {
+            throw new ConstrettoConversionException(value, clazz, e);
+        }
+    }
 }
