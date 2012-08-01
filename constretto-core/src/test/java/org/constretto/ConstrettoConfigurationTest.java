c26a9ee7014fc9ea8d53d1e42f25fa9ae18731fa
Endre Midtgård Meckelborg <endremm@finn.no>
2012-08-01 13:06:09 +0200

    Added support of command param CONSTRETTO_OVERRIDE to easily support override of properties runtime.

--- constretto-core/src/test/java/org/constretto/ConstrettoConfigurationTest.java
+++ constretto-core/src/test/java/org/constretto/ConstrettoConfigurationTest.java
@@ -1,6 +1,7 @@
 package org.constretto;
 
 import org.constretto.model.ClassPathResource;
+import org.constretto.model.Resource;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -68,4 +69,65 @@ public class ConstrettoConfigurationTest {
         assertEquals(2,props.size());
     }
 
+    @Test
+    public void getConfiguration_iniStoreWithoutOverride() {
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createIniFileConfigurationStore().addResource(Resource.create("classpath:cache1.ini")).done()
+                .getConfiguration();
+
+        assertEquals(configuration.evaluateToString("key1"), "value1");
+    }
+
+    @Test
+    public void getConfiguration_iniStoreWithOneOverride() {
+        System.setProperty(ConstrettoBuilder.OVERRIDES, "classpath:cache1-override1.ini");
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createIniFileConfigurationStore().addResource(Resource.create("classpath:cache1.ini")).done()
+                .getConfiguration();
+
+        assertEquals(configuration.evaluateToString("key1"), "value1-override1");
+        System.clearProperty(ConstrettoBuilder.OVERRIDES);
+    }
+
+    @Test
+    public void getConfiguration_iniStoreWithTwoOverrides() {
+        System.setProperty(ConstrettoBuilder.OVERRIDES, "classpath:cache1-override1.ini,classpath:cache1-override2.ini");
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createIniFileConfigurationStore().addResource(Resource.create("classpath:cache1.ini")).done()
+                .getConfiguration();
+
+        assertEquals(configuration.evaluateToString("key1"), "value1-override2");
+        System.clearProperty(ConstrettoBuilder.OVERRIDES);
+    }
+
+    @Test
+    public void getConfiguration_propertyStoreWithoutOverride() {
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createPropertiesStore().addResource(Resource.create("classpath:cache3.properties")).done()
+                .getConfiguration();
+
+        assertEquals(configuration.evaluateToString("key3"), "value3");
+    }
+
+    @Test
+    public void getConfiguration_propertyStoreWithOneOverride() {
+        System.setProperty(ConstrettoBuilder.OVERRIDES, "classpath:cache3-override1.properties");
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createPropertiesStore().addResource(Resource.create("classpath:cache3.properties")).done()
+                .getConfiguration();
+
+        assertEquals(configuration.evaluateToString("key3"), "value3-override1");
+        System.clearProperty(ConstrettoBuilder.OVERRIDES);
+    }
+
+    @Test
+    public void getConfiguration_propertyStoreWithTwoOverrides() {
+        System.setProperty(ConstrettoBuilder.OVERRIDES, "classpath:cache3-override1.properties,classpath:cache3-override2.properties");
+        ConstrettoConfiguration configuration = new ConstrettoBuilder()
+                .createPropertiesStore().addResource(Resource.create("classpath:cache3.properties")).done()
+                .getConfiguration();
+
+        assertEquals(configuration.evaluateToString("key3"), "value3-override2");
+        System.clearProperty(ConstrettoBuilder.OVERRIDES);
+    }
 }
