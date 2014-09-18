acdd615b32e73dece1e1e55766c6e4d2d8f71989
Sondre Eikanger Kvalø <zapodot@gmail.com>
2014-09-18 09:51:34 +0200

    Added test for the new default @Value feature

--- constretto-spring/src/test/java/org/constretto/spring/javaconfig/BasicConstrettoConfigurationTest.java
+++ constretto-spring/src/test/java/org/constretto/spring/javaconfig/BasicConstrettoConfigurationTest.java
@@ -18,17 +18,22 @@ import static org.junit.Assert.assertEquals;
 @ContextConfiguration(classes = BasicConstrettoConfigurationTest.TestContext.class)
 public class BasicConstrettoConfigurationTest {
 
+    public static final String DEFAULT_VALUE = "Default";
     @Configuration(required = true)
     private String key1;
 
     @Value("${key1}")
     private String key1AsValue;
 
+    @Value("${nothere:" + DEFAULT_VALUE + "}")
+    private String defaultValue;
+
     @Test
     public void testKeyConfigured() throws Exception {
         final String expectedValue = "value1";
         assertEquals(expectedValue, key1);
         assertEquals(expectedValue, key1AsValue);
+        assertEquals(DEFAULT_VALUE, defaultValue);
     }
 
     @org.springframework.context.annotation.Configuration
